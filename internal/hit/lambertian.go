c8808b705ae87e8963fd45fac42bf7427347d94a
Guillaume TECHER <guillaumeval.techer@gmail.com>
2020-04-21 22:37:47 -0400

    fresnel refraction using schlick approx for lambertian

--- internal/hit/lambertian.go
+++ internal/hit/lambertian.go
@@ -1,6 +1,9 @@
 package hit
 
 import (
+	"math"
+	"math/rand"
+
 	"github.com/GuillaumeTech/3dgo/internal/geom"
 )
 
@@ -9,6 +12,25 @@ type Lambertian struct {
 }
 
 func (l Lambertian) Scatter(rayIn geom.Ray, hitRecord *HitRecord, attenutaion *geom.Vec3d, scattered *geom.Ray) bool {
+	var etaTOverEtaI float64
+	if hitRecord.FrontFace {
+		etaTOverEtaI = 1 / 1.1
+	} else {
+		etaTOverEtaI = 1.1
+	}
+	unitDir := geom.UnitVector(rayIn.Direction)
+	cosTheta := math.Min(geom.DotProduct(unitDir.Negate(), hitRecord.Normal), 1)
+	reflectProbability := schlick(cosTheta, etaTOverEtaI)
+	randomFloat := rand.Float64()
+
+	if randomFloat < reflectProbability {
+		refelected := refelect(unitDir, hitRecord.Normal)
+		scattered.Direction = refelected
+		scattered.Origin = hitRecord.P
+		*attenutaion = l.Albedo
+		return true
+	}
+
 	scatterDir := geom.AddTwoVec(geom.RandomUnitVector(), geom.AddTwoVec(hitRecord.P, hitRecord.Normal))
 	scattered.Direction = scatterDir
 	scattered.Origin = hitRecord.P
